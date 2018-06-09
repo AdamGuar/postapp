b4b70a966009484627b314ffedffa3083d578d80
karolskora1993 <karol-skora@poczta.pl>
2018-06-09 11:16:17 +0200

    display model

--- src/main/resources/static/js/webgl.js
+++ src/main/resources/static/js/webgl.js
@@ -1,37 +1,25 @@
 function drawCanvas() {
-  const canvas = document.querySelector("#glCanvas");
+  const canvas = document.getElementById("glCanvas");
 
   var camera, scene, renderer;
   var geometry, material, mesh;
 
-  init();
-  animate();
 
-  function init() {
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  camera.position.z = 1;
 
-  	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-  	camera.position.z = 1;
+  scene = new THREE.Scene();
 
-  	scene = new THREE.Scene();
+  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  material = new THREE.MeshNormalMaterial();
 
-  	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-  	material = new THREE.MeshNormalMaterial();
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
-  	mesh = new THREE.Mesh( geometry, material );
-  	scene.add( mesh );
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  renderer.setSize(canvas.width, canvas.height);
+  canvas.appendChild(renderer.domElement);
 
-  	renderer = new THREE.WebGLRenderer( { antialias: true } );
-  	renderer.setSize( canvas.width, canvas.height );
-  	canvas.appendChild( renderer.domElement );
-  }
-
-  function animate() {
-
-  	requestAnimationFrame( animate );
-
-  	mesh.rotation.x += 0.01;
-  	mesh.rotation.y += 0.02;
-
-  	renderer.render( scene, camera );
-
-  }
+}
