2d4c9e7317ad15d8de622fee87c05eebc8bcf00c
karolskora1993 <karol-skora@poczta.pl>
2018-06-10 13:24:21 +0200

    display model

--- src/main/resources/static/js/webgl.js
+++ src/main/resources/static/js/webgl.js
@@ -1,14 +1,18 @@
+var camera, scene, renderer;
+var geometry, material, mesh;
+
 function drawCanvas() {
   const canvas = document.querySelector("#glCanvas");
 
-  var camera, scene, renderer;
-  var geometry, material, mesh;
 
 
-  camera = new THREE.PerspectiveCamera(70, 600 / 400, 0.01, 10);
+  camera = new THREE.PerspectiveCamera(70, 600 / 400, 1, 1000);
   camera.position.z = 600;
+  camera.position.y = 150;
+
 
   scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0xf0f0f0 );
 
   geometry = new THREE.BoxGeometry(200, 200, 200);
   material = new THREE.MeshNormalMaterial();
@@ -21,5 +25,15 @@ function drawCanvas() {
   });
   renderer.setSize(600, 400);
   canvas.appendChild(renderer.domElement);
+  canvas.addEventListener( 'touchmove', onDocumentTouchMove, false );
 
+
+}
+
+function onDocumentTouchMove( event ) {
+  if ( event.touches.length === 1 ) {
+    event.preventDefault();
+    mouseX = event.touches[ 0 ].pageX - windowHalfX;
+    targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
+  }
 }
