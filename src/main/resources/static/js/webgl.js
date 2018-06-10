5d3e20f4bb8154f0d2e99a3916ec8c251f494bc2
karolskora1993 <karol-skora@poczta.pl>
2018-06-10 16:06:52 +0200

    display model

--- src/main/resources/static/js/webgl.js
+++ src/main/resources/static/js/webgl.js
@@ -8,6 +8,12 @@ var windowHalfX = 600 / 2;
 var windowHalfY = 400 / 2;
 
 function drawCanvas() {
+  init();
+  render();
+  animate();
+}
+
+function init() {
   var canvas = document.getElementById('glCanvas');
 
   renderer = new THREE.WebGLRenderer({
@@ -20,24 +26,17 @@ function drawCanvas() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xFFFFF);
 
-  camera = new THREE.PerspectiveCamera(40, 600 / 400, 1, 1000);
+  camera = new THREE.PerspectiveCamera(45, 600 / 400, 1, 10000);
   camera.position.z = 200;
 
   controls = new THREE.TrackballControls(camera);
-  controls.rotateSpeed = 1.0;
-  controls.zoomSpeed = 1.2;
-  controls.panSpeed = 0.8;
-  controls.noZoom = false;
-  controls.noPan = false;
-  controls.staticMoving = true;
-  controls.dynamicDampingFactor = 0.3;
-  controls.keys = [65, 83, 68];
   controls.addEventListener('change', render);
+
   scene.add(camera);
 
 
-	var light = new THREE.AmbientLight( 0x222222 );
-	scene.add( light );
+  var light = new THREE.AmbientLight(0x222222);
+  scene.add(light);
 
 
   var geometry = new THREE.BoxGeometry(50, 50, 50);
@@ -49,10 +48,13 @@ function drawCanvas() {
   scene.add(mesh);
 
   render();
-
 }
 
 function render() {
-  controls.update();
   renderer.render(scene, camera);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
 }
