26540e72cb42810f2b65f61ad8b1fce299c99a8d
karolskora1993 <karol-skora@poczta.pl>
2018-06-10 14:35:29 +0200

    display model

--- src/main/resources/static/js/webgl.js
+++ src/main/resources/static/js/webgl.js
@@ -1,38 +1,26 @@
 var camera, scene, renderer;
-var geometry, material, mesh;
 
 function drawCanvas() {
   var canvas = document.getElementById('glCanvas');
 
 
   camera = new THREE.PerspectiveCamera(70, 600 / 400, 1, 1000);
-  camera.position.z = 600;
+  camera.position.z = 150;
 
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xe0e0e0);
 
-  // geometry = new THREE.BoxGeometry(200, 200, 200);
-  // material = new THREE.MeshNormalMaterial();
-  //
-  // mesh = new THREE.Mesh(geometry, material);
-  // scene.add(mesh);
+  var geometry = new THREE.BoxGeometry(100, 100, 100);
+  var material = new THREE.MeshBasicMaterial( color: 0x00ff00 } );
 
-  renderer = new THREE.WebGLRenderer({
-    antialias: true
-  });
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer();
 
   renderer.setSize(600, 400);
   canvas.appendChild(renderer.domElement);
-  // canvas.addEventListener( 'touchmove', onDocumentTouchMove, false );
 
 
 }
-
-// function onDocumentTouchMove( event ) {
-//   if ( event.touches.length === 1 ) {
-//     event.preventDefault();
-//     mouseX = event.touches[ 0 ].pageX - windowHalfX;
-//     targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
-//   }
-// }
