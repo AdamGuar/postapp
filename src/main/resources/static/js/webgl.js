a06d762b78a0b76ddb91fa99dc4a2096d8ec7cdc
karolskora1993 <karol-skora@poczta.pl>
2018-06-10 16:14:56 +0200

    display model

--- src/main/resources/static/js/webgl.js
+++ src/main/resources/static/js/webgl.js
@@ -30,6 +30,15 @@ function init() {
   camera.position.z = 200;
 
   controls = new THREE.TrackballControls(camera);
+  controls.rotateSpeed = 1.0;
+  controls.zoomSpeed = 1.2;
+  controls.panSpeed = 0.8;
+  controls.noZoom = false;
+  controls.noPan = false;
+  controls.staticMoving = true;
+  controls.dynamicDampingFactor = 0.3;
+  controls.keys = [65, 83, 68];
+
   controls.addEventListener('change', render);
 
   scene.add(camera);
