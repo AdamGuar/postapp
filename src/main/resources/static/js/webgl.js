4f99bc5aa9bd4c87cf23b27d004ebccf06de0c9e
karolskora1993 <karol-skora@poczta.pl>
2018-06-10 15:26:32 +0200

    display model

--- src/main/resources/static/js/webgl.js
+++ src/main/resources/static/js/webgl.js
@@ -21,8 +21,20 @@ function drawCanvas() {
   scene.background = new THREE.Color(0xe0e0e0);
 
   camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.2, 1000);
-  camera.position.z = 150;
+  camera.position.z = 300;
+
+  controls = new THREE.TrackballControls(camera);
+  controls.rotateSpeed = 1.0;
+  controls.zoomSpeed = 1.2;
+  controls.panSpeed = 0.8;
+  controls.noZoom = false;
+  controls.noPan = false;
+  controls.staticMoving = true;
+  controls.dynamicDampingFactor = 0.3;
+  controls.keys = [65, 83, 68];
+  controls.addEventListener('change', render);
   scene.add(camera);
+
 
   var geometry = new THREE.BoxGeometry(50, 50, 50);
   var material = new THREE.MeshBasicMaterial({
@@ -31,58 +43,7 @@ function drawCanvas() {
 
   var mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
-  canvas.addEventListener('mousedown', onDocumentMouseDown, false);
-  canvas.addEventListener('touchstart', onDocumentTouchStart, false);
-  canvas.addEventListener('touchmove', onDocumentTouchMove, false);
-
 
   renderer.render(scene, camera);
 
-}
-
-function onDocumentMouseDown(event) {
-  event.preventDefault();
-  canvas.addEventListener('mousemove', onDocumentMouseMove, false);
-  canvas.addEventListener('mouseup', onDocumentMouseUp, false);
-  canvas.addEventListener('mouseout', onDocumentMouseOut, false);
-  mouseXOnMouseDown = event.clientX - windowHalfX;
-  targetRotationOnMouseDown = targetRotation;
-}
-
-function onDocumentMouseMove(event) {
-  console.log('mousemove');
-  mouseX = event.clientX - windowHalfX;
-  targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.02;
-}
-
-function onDocumentMouseUp(event) {
-  console.log('mouseup');
-  canvas.removeEventListener('mousemove', onDocumentMouseMove, false);
-  canvas.removeEventListener('mouseup', onDocumentMouseUp, false);
-  canvas.removeEventListener('mouseout', onDocumentMouseOut, false);
-}
-
-function onDocumentMouseOut(event) {
-  console.log('mouseout');
-  canvas.removeEventListener('mousemove', onDocumentMouseMove, false);
-  canvas.removeEventListener('mouseup', onDocumentMouseUp, false);
-  canvas.removeEventListener('mouseout', onDocumentMouseOut, false);
-}
-
-function onDocumentTouchStart(event) {
-  console.log('touchstart');
-  if (event.touches.length === 1) {
-    event.preventDefault();
-    mouseXOnMouseDown = event.touches[0].pageX - windowHalfX;
-    targetRotationOnMouseDown = targetRotation;
-  }
-}
-
-function onDocumentTouchMove(event) {
-  console.log('touchmove');
-  if (event.touches.length === 1) {
-    event.preventDefault();
-    mouseX = event.touches[0].pageX - windowHalfX;
-    targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.05;
-  }
 }
