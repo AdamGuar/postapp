d7712224336ff6aa526045458fe47796bb214eb0
karolskora1993 <karol-skora@poczta.pl>
2018-06-10 15:05:53 +0200

    display model

--- src/main/resources/static/js/webgl.js
+++ src/main/resources/static/js/webgl.js
@@ -1,9 +1,18 @@
 var camera, scene, renderer;
 
+var targetRotation = 0;
+var targetRotationOnMouseDown = 0;
+var mouseX = 0;
+var mouseXOnMouseDown = 0;
+var windowHalfX = 600 / 2;
+var windowHalfY = 400 / 2;
+
 function drawCanvas() {
   var canvas = document.getElementById('glCanvas');
 
-  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
 
   renderer.setSize(600, 400);
   canvas.appendChild(renderer.domElement);
@@ -15,12 +24,60 @@ function drawCanvas() {
   camera.position.z = 150;
   scene.add(camera);
 
-  var geometry = new THREE.BoxGeometry(100, 100, 100);
-  var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 } );
+  var geometry = new THREE.BoxGeometry(50, 50, 50);
+  var material = new THREE.MeshBasicMaterial({
+    color: 0xFF4040
+  });
 
   var mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
+  document.addEventListener('mousedown', onDocumentMouseDown, false);
+  document.addEventListener('touchstart', onDocumentTouchStart, false);
+  document.addEventListener('touchmove', onDocumentTouchMove, false);
+
 
   renderer.render(scene, camera);
 
+}
+
+function onDocumentMouseDown(event) {
+  event.preventDefault();
+  document.addEventListener('mousemove', onDocumentMouseMove, false);
+  document.addEventListener('mouseup', onDocumentMouseUp, false);
+  document.addEventListener('mouseout', onDocumentMouseOut, false);
+  mouseXOnMouseDown = event.clientX - windowHalfX;
+  targetRotationOnMouseDown = targetRotation;
+}
+
+function onDocumentMouseMove(event) {
+  mouseX = event.clientX - windowHalfX;
+  targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.02;
+}
+
+function onDocumentMouseUp(event) {
+  document.removeEventListener('mousemove', onDocumentMouseMove, false);
+  document.removeEventListener('mouseup', onDocumentMouseUp, false);
+  document.removeEventListener('mouseout', onDocumentMouseOut, false);
+}
+
+function onDocumentMouseOut(event) {
+  document.removeEventListener('mousemove', onDocumentMouseMove, false);
+  document.removeEventListener('mouseup', onDocumentMouseUp, false);
+  document.removeEventListener('mouseout', onDocumentMouseOut, false);
+}
+
+function onDocumentTouchStart(event) {
+  if (event.touches.length === 1) {
+    event.preventDefault();
+    mouseXOnMouseDown = event.touches[0].pageX - windowHalfX;
+    targetRotationOnMouseDown = targetRotation;
+  }
+}
+
+function onDocumentTouchMove(event) {
+  if (event.touches.length === 1) {
+    event.preventDefault();
+    mouseX = event.touches[0].pageX - windowHalfX;
+    targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.05;
+  }
 }
