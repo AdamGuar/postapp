53c5d66458aebdbf1d162c784a107caffe059444
karolskora1993 <karol-skora@poczta.pl>
2018-06-12 10:27:32 +0200

    compute camera position

--- src/main/resources/static/js/webgl.js
+++ src/main/resources/static/js/webgl.js
@@ -28,7 +28,7 @@ function init() {
   scene.background = new THREE.Color(0xffffff);
 
   camera = new THREE.PerspectiveCamera(45, 600 / 400, 1, 10000);
-  camera.position.z = 0.1;
+  camera.position.z = 1;
 
   controls = new THREE.TrackballControls(camera);
   controls.rotateSpeed = 1.0;
@@ -67,6 +67,7 @@ function init() {
     const el = model.elements[i];
     if (el.nodes[1].x > max_width) max_width = el.nodes[1].x;
     if (el.nodes[3].y > max_height) max_height = el.nodes[3].y;
+    
 
     const width = el.nodes[1].x - el.nodes[0].x;
     const height = el.nodes[2].y - el.nodes[1].y;
@@ -89,8 +90,6 @@ function init() {
     mesh.position.set(el.nodes[0].x, el.nodes[0].y, el.nodes[0].z);
     scene.add(mesh);
   }
-  const max = (max_width > max_height) ? max_width: max_height;
-  console.log(max);
 
   render();
 }
